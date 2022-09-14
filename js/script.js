47272ea8d50cf2cb3e4b76c1e9796e023f3b55f5
yeshna <ymg@esokia-webagency.com>
2022-09-14 10:30:44 +0400

    web page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,5 @@ $(document).ready(function(){
     $('.toggle-button').on('click', function(){
         $('nav ul').toggleClass('showing');
     });
-    $('nav').on('mouseleave', function(){
-        $('nav ul').toggleClass('showing');
-  });
+    
 });
